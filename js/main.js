07f02d9c4bc41cd41665af769970f3d6939575c2
david <david.ber.1530@gmail.com>
2020-03-22 01:35:16 -0300

    arreglo de url

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function(w) {
   w.onload = function() {
     console.log("la pagina se cargo");
-    fetch("http swapi.co/api/planets/1/")
+    fetch("http/swapi.co/api/planets/1/")
       .then(res => res.json())
       .then(resul => console.log(resul));
   };
