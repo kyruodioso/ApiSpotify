54d0eeb1124f598e94fe53620f046854578d3e94
david <david.ber.1530@gmail.com>
2020-03-22 01:33:31 -0300

    fetch de prueba

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 (function(w) {
   w.onload = function() {
     console.log("la pagina se cargo");
+    fetch("http swapi.co/api/planets/1/")
+      .then(res => res.json())
+      .then(resul => console.log(resul));
   };
 }.call(window));
