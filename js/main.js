83c7d78417bb68c517e41f6011606bcb8eee6ca5
kyruodioso <kanu_nirvana@hotmail.com>
2020-03-22 11:25:33 -0300

    agregando nombres de personajes al documento

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+
 (function(w) {
+  let personaje = document.getElementById('personaje');
+  let personaje2 = document.getElementById('personaje2');
+
   w.onload = function() {
     console.log("la pagina se cargo");
-    fetch("https://swapi.co/api/planets/1/?format=wookiee")
+    fetch("https://swapi.co/api/people/2")
       .then(res => res.json())
-      .then(resul => console.log(resul));
+      .then(resul =>{
+        personaje.style.display ='none';
+        personaje2.innerHTML=resul.name;
+
+      } );
   };
 }.call(null, window));
